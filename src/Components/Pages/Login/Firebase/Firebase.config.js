b70686a4b8dd97d111e13fdf9284ed94d0cbec65
Md Maruf <83667585+mdmaruf641@users.noreply.github.com>
2021-11-01 23:23:45 +0600

    Delete src directory

--- src/Components/Pages/Login/Firebase/Firebase.config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBMhIejN8Gb3IkolvK0MKNTlLcu_NXsLac",
-  authDomain: "g-travels-76e25.firebaseapp.com",
-  projectId: "g-travels-76e25",
-  storageBucket: "g-travels-76e25.appspot.com",
-  messagingSenderId: "668646332119",
-  appId: "1:668646332119:web:99a7204a9bddb52d15c292",
-};
-export default firebaseConfig;
